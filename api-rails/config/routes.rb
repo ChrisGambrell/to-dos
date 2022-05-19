16b352b96a8816fe9245ed79cb999126a6aeee6e
Chris Gambrell <christopher@gambrell.info>
2022-05-19 15:46:03 -0400

    Add verify to routes

--- api-rails/config/routes.rb
+++ api-rails/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   scope 'api/v1' do
     post '/auth/login', to: 'authentication#login'
+    get '/auth/verify', to: 'authentication#verify'
     resources :todos, param: :todo_id
     resources :users, param: :user_id
   end
